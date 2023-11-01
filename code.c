94af79ddfd0ee645f2b2223534a6d2f5731ef74f
Ali Jaddoa <62108867+alijaddoacccu@users.noreply.github.com>
2023-11-01 13:05:12 +0000

    Add files via upload

---
+++ code.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // we need this library to get access to the input and out put methods for printing to terminal
+
+int main()
+{
+    printf("HHello World\n"); // lets say hello, where it all began...
+    
+    printf("Goodbye World\n"); // this seems fitting as the program will close after this.
+    
+    return 0; // returns 0 to the int of main() and terminate the program
+}
